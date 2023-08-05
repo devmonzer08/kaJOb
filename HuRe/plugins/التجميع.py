87f79cbe190e6292e2f24b63112d71d297a2fd5a
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-08-05 19:19:29 +0300

    Update التجميع.py

--- HuRe/plugins/التجميع.py
+++ HuRe/plugins/التجميع.py
@@ -412,6 +412,46 @@ async def _(event):
             await event.edit(f"القناة رقم {chs}")
 
     await l313l.send_message(event.chat_id, "تم الانتهاء من التجميع")
+@l313l.on(admin_cmd(pattern="تجميع دعمكم"))
+async def _(event):
+    await event.edit("**᯽︙سيتم تجميع النقاط من بوت العرب , قبل كل شي تأكد من انك قمت بالانضمام الى القنوات الاشتراك الاجباري للبوت لعدم حدوث اخطاء**")
+    channel_entity = await l313l.get_entity(bot_username5)
+    await l313l.send_message(bot_username5, '/start')
+    await asyncio.sleep(4)
+    msg0 = await l313l.get_messages(bot_username5, limit=1)
+    await msg0[0].click(1)
+    await asyncio.sleep(4)
+    msg1 = await l313l.get_messages(bot_username5, limit=1)
+    await msg1[0].click(0)
+
+    chs = 1
+    for i in range(100):
+        await asyncio.sleep(4)
+        list = await l313l(GetHistoryRequest(peer=channel_entity, limit=1, offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+        msgs = list.messages[0]
+        if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم بتجميع النقاط بطريقة مختلفة') != -1:
+            await l313l.send_message(event.chat_id, "تم الانتهاء من التجميع")
+            break
+        message = event.message.message
+        channel_username = re.search(r"@(\w+)", message)
+        if channel_username:
+            try:
+                channel_entity2 = await l313l.get_entity(channel_username.group(0))
+                await l313l(JoinChannelRequest(channel_entity2))
+                await event.edit("**᯽︙تم الانضمام إلى القناة بنجاح**")
+            except:
+                await event.edit("**᯽︙حدث خطأ أثناء الانضمام إلى القناة**")
+        else:
+            await event.edit("**᯽︙لم يتم العثور على اسم قناة في الرسالة**")
+            chs += 1
+            await event.edit(f"تم الانضمام في {chs} قناة")
+        except:
+            msg2 = await l313l.get_messages(bot_username5, limit=1)
+            await msg2[0].click(text='اشتركت ✅')
+            chs += 1
+            await event.edit(f"القناة رقم {chs}")
+
+    await l313l.send_message(event.chat_id, "تم الانتهاء من التجميع")
 
 @l313l.ar_cmd(pattern="راتب وعد(?:\s|$)([\s\S]*)")
 async def hussein(event):
