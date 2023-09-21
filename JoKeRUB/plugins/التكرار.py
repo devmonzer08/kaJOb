34d7b34827af86a663e2105abd5083e00d1736e3
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-09-22 00:17:07 +0300

    Update التكرار.py

--- JoKeRUB/plugins/التكرار.py
+++ JoKeRUB/plugins/التكرار.py
@@ -7,6 +7,7 @@ from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 from telethon.utils import get_display_name
 from JoKeRUB import l313l
+from telethon import events
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from ..helpers.tools import media_type
