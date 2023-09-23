530b2ef842d0ff241f379bbaa1c990602f26b2af
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-09-23 13:40:38 +0300

    Update المطور.py

--- JoKeRUB/plugins/المطور.py
+++ JoKeRUB/plugins/المطور.py
@@ -25,12 +25,12 @@ plugin_category = "utils"
 @l313l.on(events.NewMessage(pattern=r'\.event', outgoing=True))
 async def my_event_handler(event):
     if event.is_reply:
-        replied_message = await event.get_reply_message()
-        if replied_message and replied_message.text:
-            message_text = replied_message.text
-            with open("replied_message.txt", "w") as file:
-                file.write(message_text)
-            await l313l.send_file(event.chat_id, "replied_message.txt")
+    replied_message = await event.get_reply_message()
+    if replied_message:
+        event_info = await l313l.get_event_info(replied_message)
+        with open("event_info.txt", "w") as file:
+            file.write(event_info.stringify())
+        await l313l.send_file(event.chat_id, "event_info.txt")
 
 @l313l.ar_cmd(
     pattern="المطور$",
